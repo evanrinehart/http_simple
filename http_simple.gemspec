34d368114221e3d347e5dbaae4fee6982ce5cfde
Evan Rinehart <erinehart@searchinfluence.com>
2014-05-22 20:09:32 -0500

    initial commit

---
+++ http_simple.gemspec
@@ -0,0 +1,10 @@
+Gem::Specification.new do |s|
+  s.name        = 'http_simple'
+  s.version     = '0.0.1'
+  s.summary     = 'simple http client'
+  s.description = 'simple http client'
+  s.author      = "EvanR"
+  s.email       = 'evanrinehart@gmail.com'
+  s.files       = Dir['lib/*']
+  s.license     = 'MIT'
+end
---
+++ lib/http_simple.rb
@@ -0,0 +1,121 @@
+require 'net/http'
+require 'openssl'
+require 'uri'
+
+module HTTPSimple
+
+  class HTTPException < StandardError
+    def initialize report=nil
+      @report = report
+    end
+
+    def report
+      @report
+    end
+  end
+
+  class ResponseException < HTTPException; end
+  class NetworkException < HTTPException; end
+
+  class NotFound < ResponseException; end
+  class BadRequest < ResponseException; end
+  class Forbidden < ResponseException; end
+  class Unauthorized < ResponseException; end
+  class ServiceUnavailable < ResponseException; end
+  class InternalServerError < ResponseException; end
+  class BadGateway < ResponseException; end
+
+  def self.get uri_string, query:{}, headers:{}
+    generic_http_exec :get, uri_string, query, nil, headers
+  end
+
+  def self.head uri_string, query:{}, headers:{}
+    generic_http_exec :head, uri_string, query, nil, headers
+  end
+
+  def self.post uri_string, query:{}, body:{}, headers:{}
+    generic_http_exec :post, uri_string, query, body, headers
+  end
+
+  def self.generic_http_exec meth, uri_string, query, body, headers
+    uri = assert_string_uri uri_string
+    http = new_http(uri)
+    response = rethrow_network_exceptions(uri, headers:headers, body:body) do 
+      raw_uri = uri_query_to_s(uri, query)
+      case meth
+        when :get then http.get(raw_uri, headers)
+        when :head then http.head(raw_uri, headers)
+        when :post then http.post(raw_uri, body, headers)
+      end
+    end
+    assert_ok(response.code, {
+      :uri => uri_string,
+      :request_body => body,
+      :headers => headers,
+      :status => "#{response.code} #{response.message}",
+      :response_body => response.body
+    })
+    response.body
+  end
+
+  def self.assert_string_uri uri_string
+    if !uri_string.is_a?(String)
+      raise ArgumentError, "uri string expected, not #{uri_string.inspect}"
+    end
+
+    uri = URI.parse uri_string
+
+    if uri.scheme !~ /^https?$/
+      raise ArgumentError, "uri must use http:// or https://"
+    end
+
+    uri
+  end
+
+  def self.assert_ok code, error_report
+    eclass = case code
+      when /^2\d\d$/ then :none
+      when '400' then BadRequest
+      when '401' then Unauthorized
+      when '403' then Forbidden
+      when '404' then NotFound
+      when '500' then InternalServerError
+      when '502' then BadGateway
+      when '503' then ServiceUnavailable
+      else ResponseException
+    end
+
+    raise eclass.new(error_report), code if eclass != :none
+  end
+
+  def self.rethrow_network_exceptions uri, headers:{}, body:{}
+    begin
+      yield
+    rescue Timeout::Error, Errno::EINVAL, Errno::ECONNREFUSED,
+      Errno::ECONNRESET, Errno::EHOSTUNREACH, SocketError,
+      EOFError, OpenSSL::SSL::SSLError => e
+      report = {:uri => uri, :request_headers => headers, :request_body => body}
+      raise NetworkException.new(report), e.inspect
+    end
+  end
+
+  def self.encode_query params
+    URI.encode_www_form params
+  end
+
+  def self.uri_query_to_s uri, query
+    if query.empty?
+      uri.to_s
+    else
+      "#{uri.to_s}?#{encode_query query}"
+    end
+  end
+
+  def self.new_http uri
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = (uri.scheme == "https")
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    http
+  end
+
+end
